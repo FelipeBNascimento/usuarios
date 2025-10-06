ba92bcabcbb5004847a73b0f4cde13a2524607e2
Bazan <felipe_bazan3006@hotmail.com>
2025-10-06 15:07:48 -0300

    Atualizando o metodo salva usuario criptografando a senha

--- src/main/java/com/javanauta/usuario/business/UsuarioSevice.java
+++ src/main/java/com/javanauta/usuario/business/UsuarioSevice.java
@@ -2,8 +2,10 @@ package com.javanauta.usuario.business;
 
 import com.javanauta.usuario.business.converter.UsuarioConverter;
 import com.javanauta.usuario.infraesctruture.entity.UsuariosEntity;
+import com.javanauta.usuario.infraesctruture.exceptions.ConflictExceptions;
 import com.javanauta.usuario.infraesctruture.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
@@ -13,13 +15,36 @@ public class UsuarioSevice {
 
     private final UsuarioRepository usuarioRepository;
     private final UsuarioConverter usuarioConverter;
+    private final PasswordEncoder passwordEncoder;
 
 
     public UsuarioDTO salvarusuario(UsuarioDTO usuarioDTO) {
 
+        existeEmail(usuarioDTO.getEmail());
+        usuarioDTO.setSenha(passwordEncoder.encode(usuarioDTO.getSenha()));
         UsuariosEntity usuarios = usuarioConverter.parausuario(usuarioDTO);
         usuarios = usuarioRepository.save(usuarios);
         return usuarioConverter.parausuarioDTO(usuarios);
 
+    }
+
+    public boolean buscarEmail(String email) {
+
+        return usuarioRepository.existsByEmail(email);
+
+    }
+
+    public void existeEmail(String email) {
+
+        boolean existe = buscarEmail(email);
+        try {
+
+            if (existe) {
+                throw new ConflictExceptions("email ja existe");
+            }
+        } catch (ConflictExceptions e) {
+            throw new ConflictExceptions("email ja existe" + e.getCause());
+
+        }
     }
 }
